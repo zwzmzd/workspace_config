a4a4ce761197f2a5d5ce4f444aa74a8ae81361ca
zwzmzd <zwzmzd@mail.enjoy-what.com>
2026-07-14 12:31:48 +0800

    [git] track Python diff configuration

--- flush_config.sh
+++ flush_config.sh
@@ -8,8 +8,12 @@ function abs_path() {
 
 DIR=`abs_path`
 ESHELL_PROFILE=~/.emacs.d/eshell/profile
+GIT_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/git"
 ln -sf $DIR/emacs/.emacs ~/.emacs
 mkdir -p `dirname $ESHELL_PROFILE`
 ln -sf $DIR/emacs/eshell_profile $ESHELL_PROFILE
 ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/clang-format/.clang-format ~/.clang-format
+mkdir -p "$GIT_CONFIG_HOME"
+ln -sf "$DIR/git/config" "$GIT_CONFIG_HOME/config"
+ln -sf "$DIR/git/attributes" "$GIT_CONFIG_HOME/attributes"

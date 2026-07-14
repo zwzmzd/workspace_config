04b585780a0cfb8f64a3921fc88e5d01b40f2857
zwzmzd <zwzmzd@mail.enjoy-what.com>
2026-07-14 12:34:07 +0800

    [config] normalize home directory paths

--- flush_config.sh
+++ flush_config.sh
@@ -7,13 +7,13 @@ function abs_path() {
 }
 
 DIR=`abs_path`
-ESHELL_PROFILE=~/.emacs.d/eshell/profile
+ESHELL_PROFILE="$HOME/.emacs.d/eshell/profile"
 GIT_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/git"
-ln -sf $DIR/emacs/.emacs ~/.emacs
-mkdir -p `dirname $ESHELL_PROFILE`
-ln -sf $DIR/emacs/eshell_profile $ESHELL_PROFILE
-ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
-ln -sf $DIR/clang-format/.clang-format ~/.clang-format
+ln -sf "$DIR/emacs/.emacs" "$HOME/.emacs"
+mkdir -p "$(dirname "$ESHELL_PROFILE")"
+ln -sf "$DIR/emacs/eshell_profile" "$ESHELL_PROFILE"
+ln -sf "$DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DIR/clang-format/.clang-format" "$HOME/.clang-format"
 mkdir -p "$GIT_CONFIG_HOME"
 ln -sf "$DIR/git/config" "$GIT_CONFIG_HOME/config"
 ln -sf "$DIR/git/attributes" "$GIT_CONFIG_HOME/attributes"
